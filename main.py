ad7af172f3946b7815fb637cc6d12c41378a9989
ianPasq <iangrofoski@gmail.com>
2025-01-11 03:03:38 -0300

    added interpreter class

--- main.py
+++ main.py
@@ -12,3 +12,38 @@ class Token(object):
     
     def __repr__(self):
         return self.__str__()
+
+class Interpreter(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_token = None
+
+    def error(self):
+        raise Exception("parsing error")
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char =  self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+    
+    def get_next_token(self):
+        text = self.text
+
+        if self.pos > len(text) - 1:
+            return Token(EOF, None)
+
+        current_char = text[self.pos]
+
+        if current_char.isdigit():
+            token = Token(INTEGER, int(current_char))
+            self.pos += 1
+            return token
+            
+        self.error
