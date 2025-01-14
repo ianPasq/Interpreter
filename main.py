b6c448f94c82aca419eb6e3df9400eb136b64a8d
ianPasq <iangrofoski@gmail.com>
2025-01-13 23:44:34 -0300

    lexer function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
+INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, EOF = (
+'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', '(', ')', 'EOF'
+)
 
 class Token(object):
     def __init__(self, type, value):
@@ -13,37 +15,121 @@ class Token(object):
     def __repr__(self):
         return self.__str__()
 
-class Interpreter(object):
+class Lexer(object):
     def __init__(self, text):
         self.text = text
         self.pos = 0
-        self.current_token = None
+        self.current_char = self.text[self.pos]
 
     def error(self):
-        raise Exception("parsing error")
+        raise Exception('Invalid character')
 
     def advance(self):
         self.pos += 1
         if self.pos > len(self.text) - 1:
-            self.current_char = None
+            self.current_char = None # Indicates end of input
         else:
-            self.current_char =  self.text[self.pos]
+            self.current_char = self.text[self.pos]
 
     def skip_whitespace(self):
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
-    
-    def get_next_token(self):
-        text = self.text
 
-        if self.pos > len(text) - 1:
-            return Token(EOF, None)
+    def skip_comment(self):
+        while self.current_char != '}':
+            self.advance()
+        self.advance()
 
-        current_char = text[self.pos]
+    def number(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        if self.current_char == '.':
+            result += self.current_char
+            self.advance()
+        while (
+            self.current_char is not None and
+            self.current_char.isdigit()
+        ):
+            result += self.current_char
+            self.advance()
+            token = Token('REAL_CONST', float(result))
+        else:
+            token = Token('INTEGER_CONST', int(result))
+        return token
 
-        if current_char.isdigit():
-            token = Token(INTEGER, int(current_char))
-            self.pos += 1
+    def peek(self):
+        peek_pos = self.pos + 1
+        if peek_pos > len(self.text) - 1:
+            return None
+        else:
+            return self.text[peek_pos]
+            RESERVED_KEYWORDS = {
+            'PROGRAM': Token('PROGRAM', 'PROGRAM'),
+            'VAR': Token('VAR', 'VAR'),
+            'DIV': Token('INTEGER_DIV', 'DIV'),
+            'INTEGER': Token('INTEGER', 'INTEGER'),
+            'REAL': Token('REAL', 'REAL'),
+            'BEGIN': Token('BEGIN', 'BEGIN'),
+            'END': Token('END', 'END'),
+            }
+    def _id(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isalnum():
+            result += self.current_char
+            self.advance()
+            token = RESERVED_KEYWORDS.get(result, Token(ID, result))
             return token
-            
-        self.error
+        def get_next_token(self):
+            while self.current_char is not None:
+                if self.current_char.isalpha():
+                    return self._id()
+                if self.current_char == '{':
+                    self.advance()
+                    self.skip_comment()
+                continue
+            if self.current_char == ':' and self.peek() == '=':
+
+                self.advance()
+                self.advance()
+                return Token(ASSIGN, ':=')
+            if self.current_char == ';':
+                self.advance()
+                return Token(SEMI, ';')
+            if self.current_char == '.':
+                self.advance()
+                return Token(DOT, '.')
+            if self.current_char.isdigit():
+                return self.number()
+            if self.current_char == ':':
+                self.advance()
+                return Token(COLON, ':')
+            if self.current_char == ',':
+                self.advance()
+                return Token(COMMA, ',')
+            if self.current_char.isspace():
+                self.skip_whitespace()
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+            if self.current_char == '*':
+                self.advance()
+                return Token(MUL, '*')
+            if self.current_char == '/':
+                self.advance()
+                return Token(FLOAT_DIV, '/')
+            if self.current_char == '(':
+                self.advance()
+                return Token(LPAREN, '(')
+            if self.current_char == ')':
+                self.advance()
+                return Token(RPAREN, ')')
+            self.error()
+
+        return Token(EOF, None)
